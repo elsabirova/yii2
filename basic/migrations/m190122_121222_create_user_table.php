aa3b3d0570e2e5e3396f6fa9a3691787e002d398
elsabirova <grow_happy@mail.ru>
2019-01-22 13:31:48 +0100

    create Migration for create table User

---
+++ basic/migrations/m190122_121222_create_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m190122_121222_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string(255)->notNull(),
+            'password_hash' => $this->string(255)->notNull(),
+            'auth_key' => $this->string(255)->notNull(),
+            'creator_id' => $this->integer()->notNull(),
+            'updater_id' => $this->integer(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
