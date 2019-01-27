9efcc64f0fe851bbe2478eb2eb7f799f0cc439ca
elsabirova <grow_happy@mail.ru>
2019-01-27 19:46:16 +0100

    create Migration add_foreign_keys

---
+++ basic/migrations/m190127_182120_add_foreign_keys.php
@@ -0,0 +1,73 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m190127_182120_add_foreign_keys
+ */
+class m190127_182120_add_foreign_keys extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->addForeignKey(
+            'fk-task_user-user_id',
+            'task_user',
+            'user_id',
+            'user',
+            'id'
+        );
+
+        $this->addForeignKey(
+            'fk-task_user-task_id',
+            'task_user',
+            'task_id',
+            'task',
+            'id'
+        );
+
+        $this->addForeignKey(
+            'fk-task-creator_id',
+            'task',
+            'creator_id',
+            'user',
+            'id'
+        );
+
+        $this->addForeignKey(
+            'fk-task-updater_id',
+            'task',
+            'updater_id',
+            'user',
+            'id'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropForeignKey(
+            'fk-task_user-user_id',
+            'task_user'
+        );
+
+        $this->dropForeignKey(
+            'fk-task_user-task_id',
+            'task_user'
+        );
+
+        $this->dropForeignKey(
+            'fk-task-creator_id',
+            'task_'
+        );
+
+        $this->dropForeignKey(
+            'fk-task-updater_id',
+            'task'
+        );
+    }
+}
