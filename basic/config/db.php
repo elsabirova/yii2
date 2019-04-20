7dcf48abd2283a9f8085154ac024f2f2ff1d707d
elsabirova <grow_happy@mail.ru>
2019-04-20 11:55:18 +0200

    Database for production

--- basic/config/db.php
+++ basic/config/db.php
@@ -1,6 +1,20 @@
 <?php
 
+//Database for production
 return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=vlvlnl1grfzh34vj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=r3xxuc3lf5dqnob0',
+    'username' => 'r3xxuc3lf5dqnob0',
+    'password' => 'dibi2jer4fsj2v1n',
+    'charset' => 'utf8',
+    // Schema cache options (for production environment)
+    'enableSchemaCache' => true,
+    'schemaCacheDuration' => 60,
+    'schemaCache' => 'cache',
+];
+
+//Database for development
+/*return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
@@ -11,4 +25,4 @@ return [
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
     //'schemaCache' => 'cache',
-];
+];*/
