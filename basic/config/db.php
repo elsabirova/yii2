c5b786accd153f0a065d2aa8d90ca40a6ab70986
elsabirova <grow_happy@mail.ru>
2019-04-20 18:03:46 +0200

    Change database config

--- basic/config/db.php
+++ basic/config/db.php
@@ -1,28 +1,30 @@
 <?php
 
 //Database for production
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=vlvlnl1grfzh34vj.chr7pe7iynqr.eu-west-1.rds.amazonaws.com;dbname=r3xxuc3lf5dqnob0',
-    'username' => 'r3xxuc3lf5dqnob0',
-    'password' => 'dibi2jer4fsj2v1n',
-    'charset' => 'utf8',
-    // Schema cache options (for production environment)
-    /*'enableSchemaCache' => true,
-    'schemaCacheDuration' => 60,
-    'schemaCache' => 'cache',*/
-];
+$db = parse_url(getenv('JAWSDB_URL'));
+if ($_SERVER['SERVER_NAME'] == "secure-spire-32930.herokuapp.com") {
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=' . $db["host"] . ';dbname=' . ltrim($db["path"], '/'),
+        'username' => $db["user"],
+        'password' => $db["pass"],
+        'charset' => 'utf8',
+        // Schema cache options (for production environment)
+        'enableSchemaCache' => true,
+        'schemaCacheDuration' => 60,
+        'schemaCache' => 'cache',
+    ];
+} else {
+    //Database for development
+    return [
+        'class' => 'yii\db\Connection',
+        'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+        'username' => 'root',
+        'password' => '',
+        'charset' => 'utf8',
+    ];
+}
 
-//Database for development
-/*return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
 
-    // Schema cache options (for production environment)
-    //'enableSchemaCache' => true,
-    //'schemaCacheDuration' => 60,
-    //'schemaCache' => 'cache',
-];*/
+
+
